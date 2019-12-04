0291cc8569768a44611fa771ad3cbe22e360b931
Paulo-Not <ph744915@gmail.com>
2019-12-04 16:43:46 -0300

    Complemento da tela do Cadastro de novas turmas

--- classes/Turma.php
+++ classes/Turma.php
@@ -2,6 +2,11 @@
 
 class Turma{
 
+    public $turma;
+    public $horario;
+    public $horaEntrada;
+    public $horaSaida;
+
     public function listarTurma(){
         $conexao = new PDO("mysql:host=127.0.0.1;dbname=escola","root","");
         
@@ -11,6 +16,13 @@ class Turma{
         
         return $lista;
     }
+
+    public function inserirTurma($turma, $horario, $horaEntrada, $horaSaida){
+        $conexao = new PDO("mysql:host=127.0.0.1;dbname=escola","root","");
+        $query="INSERT INTO tb_turma(sala, periodo, horarioInicio, horarioFim) VALUES ('".$turma."', '".$horario."', '".$horaEntrada."', '".$horaSaida."')";
+        $conexao->exec($query);
+    }
+
 
 
 }
--- turmas-criar.php
+++ turmas-criar.php
@@ -5,7 +5,7 @@
 
 <h2>Cadastrar Nova Turma</h2>
 <br>
-<form action="recursos-criar-post.php" method="post">
+<form action="turmas-criar-post.php" method="post">
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
 
@@ -14,18 +14,21 @@
                 <input name="sala" type="text" class="form-control" placeholder="Sala da nova turma" required>
             </div>
 
-            
+                
+
 
             <div class="form-group">
-                <label for="periodo">Periodo</label>
-                <select class="form-control" name="perido>">
-                    <option value="">Diurno Matutino</option>
-                    <option value="">Diurno Vespertino</option>
-                    <option value="">Noturno</option>
-
+                <label for="horario">Periodo</label>
+                <select class="form-control" name="horario">
+                    <option><?php echo 'Diurno Matutino';?></option>
+                    <option><?php echo 'Diurno Vespertino';?></option>
+                    <option><?php echo 'Noturno';?></option>
+                    
                 </select> 
             </div>
-            <!--
+
+        <!--Escolha dos Horarios 
+            Implatação futura Java Scrip
             <div class="form-group">
                 <label for="horarioEntrada">Horario de Entrada</label>
                 <input name="horarioEntrada" type="time" class="form-control" required>
@@ -35,7 +38,7 @@
                 <label for="horarioSaida">Horario de Entrada</label>
                 <input name="horarioSaida" type="time" class="form-control" required>
             </div>
--->
+        -->
             <input type="submit" class="btn btn-success btn-block" value="Salvar">
         </div>
     </div>
